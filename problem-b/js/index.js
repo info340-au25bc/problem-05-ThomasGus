1ff866be9087eec5bde6bfa6c83778f1e372fb37
ThomasGus <tgus@uw.edu>
2025-10-24 12:02:53 -0700

    complete a b c

--- problem-b/js/index.js
+++ problem-b/js/index.js
@@ -3,16 +3,22 @@
 /* Define a function `addFour()` that takes a single argument 
    and returns a value 4 greater than the input.*/
 
+   function addFour(i) {
+      return i + 4;
+   }
    
 /* Create and log a variable `twelve` that is the result of passing 8 to your
    addFour() function. */
 
-   
+  let twelve = addFour(8);
+  console.log(twelve);
+
 /* Create and log a variable `twelveString` that is the result of passing "8" 
    (a string) to your addFour() function. Consider what this tells you about how
   the function should be explained (e.g., in a comment). */
 
-  
+  let twelveString = addFour("8");
+  console.log(twelveString);
 
 /* Define a function `compoundInterest()` that takes three parameters: 
      1. an initial bank balance (principle, in dollars)
@@ -26,7 +32,9 @@
      http://www.mathwarehouse.com/calculators/continuous-compound-interest-calculator.php
 */
 
-
+function compoundInterest(principle, rate, years) {
+    return principle * Math.exp(rate * years);
+}
 
 /* Define a function `fizzBuzz()` that takes in a single number as an argument.
    The function should *return* an _array_ of numbers from 1 to the argument. 
@@ -36,7 +44,22 @@
    should contain "FizzBuzz" instead of the number.
    The returned array should be empty for arguments less than 1. */
 
-   
+  function fizzBuzz(i) {
+    let final = [];
+    for (let n = 1; n <= i; n++) {
+      if (n % 3 === 0 && n % 5 === 0) {
+        final.push("FizzBuzz");
+      } else if (n % 3 === 0) {
+        final.push("Fizz");
+      } else if (n % 5 === 0) {
+        final.push("Buzz");
+      } else {
+        final.push(n);
+      }
+    }
+    return final;
+  }
+
 
 /* Define a function `getLetterFrequencies()` that takes in a single string as 
    an argument. The function should *return* an Object whose keys are characters
@@ -48,7 +71,18 @@
    for if the letter is not in the Object yet!
    You can test this method with a word like "Mississippi". */
 
-   
+   function getLetterFrequencies(str) {
+    let final = {};
+    for (let i = 0; i < str.length; i++) {
+      let char = str[i];
+      if (final[char]) {
+        final[char] += 1;
+      } else {
+        final[char] = 1;
+      }
+    }
+    return final;
+   }
 
 /* Create a variable `deck` that represents a deck of modern playing cards
    (https://en.wikipedia.org/wiki/Playing_card). This variable should be an
@@ -62,7 +96,13 @@
     
     You can log out the `deck` to check your work! */
 
-    
+    let deck = [];
+    let suits = ["hearts", "diamonds", "clubs", "spades"];
+    for (let suit of suits) {
+      for (let rank = 2; rank <= 14; rank++) {
+        deck.push({suit: suit, rank: rank});
+      }
+    }
 
 //You can test the below functions by creating e.g., a `pokerHand` array that 
 //contains five cards from the `deck`.
@@ -72,19 +112,42 @@
    is in that array.
    Hint: use a loop to check each card. */
 
-   
+   function containsQueenOfHearts(hand) {
+    for (let card of hand) {
+      if (card.suit === "hearts" && card.rank === 12) {
+        return true;
+      }
+    }
+    return false;
+  }
 
 /* Define a function `getHighCard()` that takes in an array of "card" objects
   and returns the card object with the highest value. The "high card" is the one
   with the highest rank. Cards of different suits but the same rank are 
   considered to have the same value, and either is a valid result */
 
-  
+  function getHighCard(hand) {
+    let highCard = hand[0];
+    for (let card of hand) {
+      if (card.rank > highCard.rank) {
+        highCard = card;
+      }
+    }
+    return highCard;
+  }
 
 /* Define a function `isFlush()` that takes in an array of "card" objects and
    returns whether or not the cards all have the same _suit_. */
 
-   
+   function isFlush(hand) {
+    let suit = hand[0].suit;
+    for (let card of hand) {
+      if (card.suit !== suit) {
+        return false;
+      }
+    }
+    return true;
+   }
 
 /* Extra challenge: define a function `hasPair()` that takes in an array of 
    "card" objects and returns whether or not there is at least one _pair_ (two 
@@ -92,7 +155,23 @@
    Double challenge: return the rank of the pair of cards with the highest rank 
    (e.g., if the hand contains more than one pair!) */
 
-
+   /*
+   function hasPair(hand) {
+    let highPair = -1;
+    let currPair = -1;
+    for (let card of hand) {
+      for (let i = hand.indexOf(card) + 1; i < hand.length; i++) {
+        if (card.rank === hand[i].rank) {
+          currPair = card.rank;
+          if (currPair > highPair) {
+            highPair = currPair;
+          }
+        }
+      }
+    }
+    return highPair;
+   }
+  */
 
 //Make functions and variables available to tester. DO NOT MODIFY THIS.
 if(typeof module !== 'undefined' && module.exports){
--- problem-c/js/index.js
+++ problem-c/js/index.js
@@ -38,42 +38,82 @@ let huskyGames2016 = [
 //UW's opponent (whether or not that was the home team!)
 //You can test this by passing in an individual element from the array.
 
+function extractOpponent(game) {
+  if (game.home === "UW") {
+    return game.opponent;
+  } else {
+    return game.home;
+  }
+}
 
 //Use the `map()` method and your `extractOpponent()` function to create an array
 //of UW's opponents for the season (in the same order as in the `huskyGames2016`).
 //The opponents in the list do not need to be unique.
 //Log out the opponents array.
 
+let opponents = huskyGames2016.map(extractOpponent);
+console.log(opponents);
 
 //Define a function `huskiesLost()` that takes in a "game" object and returns
 //whether or not UW lost.
 
+function huskiesLost(game) {
+  if (game.home === "UW") {
+    return game.home_score < game.opponent_score;
+  } else {
+    return game.opponent_score < game.home_score;
+  }
+}
 
 //Use the `filter()` method to create an array of games that UW lost (a smaller
 //array than the games they won!)
 //Log out the array of lost games.
 
+let lost = huskyGames2016.filter(huskiesLost);
+console.log(lost);
 
 //Log out an array of opponents that UW lost to. Hint: Use the `.map()` method 
 //to extract the opponent names!
 
+let number1opps = lost.map(extractOpponent);
+console.log(number1opps);
 
 //Use a `forEach()` loop to log out each of the games UW lost, each on its own 
 //line, in the following format:
 //    "Rutgers at UW, 13 to 48"
 //You should use an anonymous callback function.
 
+lost.forEach(function(game) {
+  if (game.home === "UW") {
+    console.log(`${game.opponent} at UW, ${game.opponent_score} to ${game.home_score}`);
+  } else {
+    console.log(`UW at ${game.home}, ${game.home_score} to ${game.opponent_score}`);
+  }
+});
 
 //Use the `filter()` method with an anonymous callback function to get an array
 //of games where UW had at least one fumble.
 //Log out HOW MANY games included fumbles.
 
+let fumbleGames = huskyGames2016.filter(function(game) {
+  return game.fumbles > 0;
+});
+console.log(fumbleGames.length); 
 
 //Define a function `mostYardsPassing()` that takes in two "game" objects and
 //returns the game that has a greater number of passing yards.
 //Your function should handle the case where the _first_ game has no 
 //`passing_yards` property, in which case it should return the second game.
 
+function mostYardsPassing(game1, game2) {
+  if (!game1.passing_yards) {
+    return game2;
+  } else if (game1.passing_yards > game2.passing_yards) {
+    return game1;
+  } else {
+    return game2;
+  }
+}
 
 //Create a variable `mostPassingGame` that refers to the "game" that had the most
 //passing yards in the season. Use the `reduce()` method with `mostYardsPassing()`
@@ -84,7 +124,8 @@ let huskyGames2016 = [
 //
 //Log out the game with the most passing yards.
 
-
+let mostPassingGame = huskyGames2016.reduce(mostYardsPassing, {});
+console.log(mostPassingGame);
 
 //It would be useful to be able to apply multiple "filter criteria" to an array
 //of games at once.
@@ -95,6 +136,12 @@ let huskyGames2016 = [
 //callback functions and "anding" (&&) the results. The `makeCombinedFilter()` 
 //function should then return this new function.
 
+function makeCombinedFilter(filter1, filter2) {
+  function combinedFilter(game) {
+    return filter1(game) && filter2(game);
+  }
+  return combinedFilter;
+}
 
 //Create a variable `fumbledAndLostFilter` which is the result of calling the 
 //`makeCombinedFilter()` function and passing two callback functions: 
@@ -103,12 +150,16 @@ let huskyGames2016 = [
 //callback like you used earlier).
 //Note that `fumbledAndLostFilter` _is_ a function!
 
+let fumbledAndLostFilter = makeCombinedFilter(huskiesLost, function(game) {
+  return game.fumbles > 0;
+});
 
 //Create an array of games that UW lost with fumbles. Use the 
 //`fumbledAndLostFilter()` function as a callback to the `filter()` method.
 //Log out the array of games lost with fumbles.
 
-
+let lostWithFumbles = huskyGames2016.filter(fumbledAndLostFilter);
+console.log(lostWithFumbles);
 
 //OPTIONAL extra practice: create a variable `avgScoreDifference` that
 //represents the average number of points UW scored _over_ their opponent
